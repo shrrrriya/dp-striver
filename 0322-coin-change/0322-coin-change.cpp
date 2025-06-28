6d14684ee4c17e76fb390d353024573d6771e908
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-06-28 22:40:23 +0200

    Time: 45 ms (39.92%), Space: 48.8 MB (41.66%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int k= func(coins.size()-1,amount,coins,dp);
+        int n=coins.size();
+        vector<vector<int>> dp(coins.size(),vector<int>(amount+1,INT_MAX));
+        
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0]==0) dp[0][target]=target/coins[0];
+        }
+
+        for(int ind=1;ind<n;ind++){
+            for(int target=0;target<=amount;target++){
+            int pick=INT_MAX;
+            if(target>=coins[ind]){
+            int res = dp[ind][ target - coins[ind]];
+            if (res != INT_MAX) pick = 1 + res;}
+            int notpick= 0 + dp[ind-1][target];
+             dp[ind][target]=min(pick,notpick);
+             }  
+        }
+        int k= dp[n-1][amount];
         if(k==INT_MAX) return -1;
         return k;
     }
- int func(int ind,int target,vector<int>& coins,vector<vector<int>>& dp){
-    if(ind==0){
-        if(target % coins[ind]==0) return target/coins[ind];
-        else return INT_MAX;
-    }
-
-    if(dp[ind][target]!=-1) return dp[ind][target];
-    int pick=INT_MAX;
-    if(target>=coins[ind]){
-        int res = func(ind, target - coins[ind], coins, dp);
-        if (res != INT_MAX) pick = 1 + res;
-    }
-    int notpick= 0 +func(ind-1,target,coins,dp);
-    return dp[ind][target]=min(pick,notpick);
-}
 };
 
 //MEMOIZATION 
@@ -40,7 +41,10 @@ public:
 
 //     if(dp[ind][target]!=-1) return dp[ind][target];
 //     int pick=INT_MAX;
-//     if(target>=coins[ind]) pick=1+func(ind,target-coins[ind],coins,dp);
+//     if(target>=coins[ind]){
+//         int res = func(ind, target - coins[ind], coins, dp);
+//         if (res != INT_MAX) pick = 1 + res;
+//     }
 //     int notpick= 0 +func(ind-1,target,coins,dp);
 //     return dp[ind][target]=min(pick,notpick);
 // }
