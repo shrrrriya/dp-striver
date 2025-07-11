f7e19ec6acfbc273f4f786dd4bf6e944720d643d
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-11 21:09:10 +0200

    Time: 41 ms (26.87%), Space: 27.5 MB (28.31%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -0,0 +1,34 @@
+//MEMOIZATION
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return func(n,m,text1,text2,dp);
+    }
+
+    int func(int ind1,int ind2,string& s1, string& s2,vector<vector<int>>& dp){
+        if(ind1<=0 || ind2<=0) return 0;
+        if(dp[ind1][ind2]!=-1) return dp[ind1][ind2];
+        if(s1[ind1-1]==s2[ind2-1]) return dp[ind1][ind2]=1+func(ind1-1,ind2-1,s1,s2,dp);
+        return dp[ind1][ind2]=max(func(ind1-1,ind2,s1,s2,dp),func(ind1,ind2-1,s1,s2,dp));
+    }
+};
+
+//RECURSION 
+// class Solution {
+// public:
+//     int longestCommonSubsequence(string text1, string text2) {
+//         int n=text1.size();
+//         int m=text2.size();
+//         return func(n,m,text1,text2);
+//     }
+
+//     int func(int ind1,int ind2,string& s1, string& s2){
+//         if(ind1<=0 || ind2<=0) return 0;
+
+//         if(s1[ind1-1]==s2[ind2-1]) return 1+func(ind1-1,ind2-1,s1,s2);
+//         return max(func(ind1-1,ind2,s1,s2),func(ind1,ind2-1,s1,s2));
+//     }
+// };
