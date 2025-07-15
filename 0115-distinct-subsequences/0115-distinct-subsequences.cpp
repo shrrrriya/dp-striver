bae08ee89e3b9ed81547c62469b0a373950c65c4
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-15 21:06:19 +0200

    Time: 25 ms (73.7%), Space: 26.9 MB (71.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -11,7 +11,7 @@ public:
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==t[j-1]){
-                    dp[i][j]=(long long int)dp[i-1][j-1]+dp[i-1][j];
+                    dp[i][j]=(long long)dp[i-1][j-1]+dp[i-1][j];
                 }
                 else{
                     dp[i][j]=dp[i-1][j];
