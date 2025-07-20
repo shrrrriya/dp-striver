c13823d389e295684bdea75a6f430c3e960e4c56
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-20 10:14:27 +0200

    Time: 7 ms (21.69%), Space: 19.5 MB (38.32%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,58 @@
+//TABULATION
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return func(0,0,prices,dp);
-    }
-    int func(int ind,bool buy,vector<int>& prices,vector<vector<int>>& dp){
-        if(ind==prices.size()) return 0; //end of the array
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
 
-        if(dp[ind][buy]!=-1) return dp[ind][buy];
-
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+        int profit=0;
         if(buy){
             //take 
-            int profit1=prices[ind]+func(ind+1,buy-1,prices,dp);
+            int profit1=-prices[ind]+dp[ind+1][0];
             //not take
-            int profit2=func(ind+1,buy,prices,dp);
-            return dp[ind][buy]=max(profit1,profit2);
+            int profit2=dp[ind+1][buy];
+            profit=max(profit,max(profit1,profit2));
         }else{
             //take
-            int p1=-prices[ind]+func(ind+1,buy-1,prices,dp);
+            int p1=prices[ind]+dp[ind+1][1];
             //not take
-            int p2=func(ind+1,buy,prices,dp);
-            return dp[ind][buy]=max(p1,p2);
+            int p2=dp[ind+1][buy];
+            profit=max(profit,max(p1,p2));
         }
+        dp[ind][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
+
+//MEMOIZATION
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<vector<int>> dp(n,vector<int>(2,-1));
+//         return func(0,0,prices,dp);
+//     }
+//     int func(int ind,bool buy,vector<int>& prices,vector<vector<int>>& dp){
+//         if(ind==prices.size()) return 0; //end of the array
+
+//         if(dp[ind][buy]!=-1) return dp[ind][buy];
+
+//         if(buy){
+//             //take 
+//             int profit1=prices[ind]+func(ind+1,buy-1,prices,dp);
+//             //not take
+//             int profit2=func(ind+1,buy,prices,dp);
+//             return dp[ind][buy]=max(profit1,profit2);
+//         }else{
+//             //take
+//             int p1=-prices[ind]+func(ind+1,buy-1,prices,dp);
+//             //not take
+//             int p2=func(ind+1,buy,prices,dp);
+//             return dp[ind][buy]=max(p1,p2);
+//         }
+//     }
+// };
