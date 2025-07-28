9df7c683a91ff14b171f824382e864e4190da5a2
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-28 20:26:01 +0200

    Time: 37 ms (61.23%), Space: 18.6 MB (23.8%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -7,42 +7,23 @@
  * };
  */
 
- //
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        //CALCULATE THE LENGTH OF headA
-        ListNode*temp=headA;
-        int sizeA=0;
-        while(temp!=NULL){
-            sizeA++;
-            temp=temp->next;
+        if (!headA || !headB) return NULL;
+
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+
+        while (t1 != t2) {
+            t1 = (t1 == NULL) ? headB : t1->next;
+            t2 = (t2 == NULL) ? headA : t2->next;
         }
-        temp=headB;
-        int sizeB=0;
-        while(temp!=NULL){
-            sizeB++;
-            temp=temp->next;
-        }
-        if(sizeA>sizeB){
-            int n=sizeA-sizeB;
-            for(int i=0;i<n;i++){
-                headA=headA->next;
-            }
-        }else{
-            int n=sizeB-sizeA;
-            for(int i=0;i<n;i++){
-                headB=headB->next;
-            }   
-        }
-        while(headA!=NULL){
-            if(headA==headB) return headA;
-            headA=headA->next;
-            headB=headB->next;
-        }
-        return NULL;
+
+        return t1; // either intersection node or NULL
     }
 };
+
 
 //BRUTE FORCE 
 // class Solution {
@@ -55,6 +36,43 @@ public:
 //         }
 //         while(headB!=NULL){
 //             if(mpp[headB]==1) return headB;
+//             headB=headB->next;
+//         }
+//         return NULL;
+//     }
+// };
+
+//OPTIMISED
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         //CALCULATE THE LENGTH OF headA
+//         ListNode*temp=headA;
+//         int sizeA=0;
+//         while(temp!=NULL){
+//             sizeA++;
+//             temp=temp->next;
+//         }
+//         temp=headB;
+//         int sizeB=0;
+//         while(temp!=NULL){
+//             sizeB++;
+//             temp=temp->next;
+//         }
+//         if(sizeA>sizeB){
+//             int n=sizeA-sizeB;
+//             for(int i=0;i<n;i++){
+//                 headA=headA->next;
+//             }
+//         }else{
+//             int n=sizeB-sizeA;
+//             for(int i=0;i<n;i++){
+//                 headB=headB->next;
+//             }   
+//         }
+//         while(headA!=NULL){
+//             if(headA==headB) return headA;
+//             headA=headA->next;
 //             headB=headB->next;
 //         }
 //         return NULL;
