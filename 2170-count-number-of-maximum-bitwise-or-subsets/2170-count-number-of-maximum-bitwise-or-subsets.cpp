9ab94a3115075891828b952e2b2e3bf7c15562d1
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-28 18:09:34 +0200

    Time: 1367 ms (5.07%), Space: 375.8 MB (7%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -5,18 +5,6 @@ public:
         //generate all subsets of nums then check for the condition 
         int maxi=INT_MIN;
         int subsets=1<<n;
-        for(int num=0;num<subsets;num++){
-            vector<int> arr;
-            for(int i=0;i<=n-1;i++){
-                if(num & (1<<i)) arr.push_back(nums[i]);
-            }
-            //compute the or of all elemnts in that array 
-            int OR=0;
-            for(int i=0;i<arr.size();i++){
-                OR=OR | arr[i];
-            }
-            maxi=max(maxi,OR);
-        }
         int cnt=0;
         for(int num=0;num<subsets;num++){
             vector<int> arr;
@@ -28,8 +16,25 @@ public:
             for(int i=0;i<arr.size();i++){
                 OR=OR | arr[i];
             }
+            if(OR>maxi){
+                 maxi=OR; cnt=0;
+            }
             if(OR==maxi) cnt++;
         }
         return cnt;
+        // int cnt=0;
+        // for(int num=0;num<subsets;num++){
+        //     vector<int> arr;
+        //     for(int i=0;i<=n-1;i++){
+        //         if(num & (1<<i)) arr.push_back(nums[i]);
+        //     }
+        //     //compute the or of all elemnts in that array 
+        //     int OR=0;
+        //     for(int i=0;i<arr.size();i++){
+        //         OR=OR | arr[i];
+        //     }
+        //     if(OR==maxi) cnt++;
+        // }
+        // return cnt;
     }
 };
