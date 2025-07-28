1ecdf9a45696c3dcad82c312ecfc00c0c853883b
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-07-28 19:00:11 +0200

    Time: 36 ms (66.6%), Space: 18.6 MB (53.06%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,18 +6,57 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+ //
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        map<ListNode*,int> mpp;
-        while(headA!=NULL){
-            mpp[headA]=1;
-            headA=headA->next;
+        //CALCULATE THE LENGTH OF headA
+        ListNode*temp=headA;
+        int sizeA=0;
+        while(temp!=NULL){
+            sizeA++;
+            temp=temp->next;
         }
-        while(headB!=NULL){
-            if(mpp[headB]==1) return headB;
+        temp=headB;
+        int sizeB=0;
+        while(temp!=NULL){
+            sizeB++;
+            temp=temp->next;
+        }
+        if(sizeA>sizeB){
+            int n=sizeA-sizeB;
+            for(int i=0;i<n;i++){
+                headA=headA->next;
+            }
+        }else{
+            int n=sizeB-sizeA;
+            for(int i=0;i<n;i++){
+                headB=headB->next;
+            }   
+        }
+        while(headA!=NULL){
+            if(headA==headB) return headA;
+            headA=headA->next;
             headB=headB->next;
         }
         return NULL;
     }
 };
+
+//BRUTE FORCE 
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         map<ListNode*,int> mpp;
+//         while(headA!=NULL){
+//             mpp[headA]=1;
+//             headA=headA->next;
+//         }
+//         while(headB!=NULL){
+//             if(mpp[headB]==1) return headB;
+//             headB=headB->next;
+//         }
+//         return NULL;
+//     }
+// };
