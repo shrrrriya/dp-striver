028605668e3f4a0593b88693edf51b620fa08c6a
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-08-03 23:28:38 +0530

    Time: 4 ms (60.03%), Space: 10.7 MB (92.01%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,7 +8,7 @@ public:
         while(r<n){
             hash[s[r]-'A']++;
             maxFreq=max(maxFreq,hash[s[r]-'A']);
-            while((r-l+1)-maxFreq>k){
+            if((r-l+1)-maxFreq>k){
                 hash[s[l]-'A']--;
                 maxFreq=0;
                 for(int i=0;i<26;i++) maxFreq=max(maxFreq,hash[i]);
@@ -22,3 +22,29 @@ public:
         return maxLen;
     }
 };
+
+//optimised
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int n=s.size();
+//         int l=0,r=0,maxLen=0,maxFreq=0;
+//         vector<int> hash(26,0);
+
+//         while(r<n){
+//             hash[s[r]-'A']++;
+//             maxFreq=max(maxFreq,hash[s[r]-'A']);
+//             while((r-l+1)-maxFreq>k){
+//                 hash[s[l]-'A']--;
+//                 maxFreq=0;
+//                 for(int i=0;i<26;i++) maxFreq=max(maxFreq,hash[i]);
+//                 l=l+1;
+//             }
+//             if((r-l+1)-maxFreq<=k){
+//                 maxLen=max(maxLen,(r-l+1));
+//             }
+//             r++;
+//         }
+//         return maxLen;
+//     }
+// };
