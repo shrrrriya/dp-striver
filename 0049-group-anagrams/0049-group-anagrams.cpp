a9dbc6e53a2722deac06337389d519ba6ddaeeb8
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-08-19 14:29:43 +0530

    Time: 36 ms (19.16%), Space: 36 MB (7.3%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        int n = strs.size();
-        vector<vector<string>> ans;
-        vector<char> used(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            vector<string> temp;
-            if (used[i]) continue;
-            temp.push_back(strs[i]);
-            // put everything into the first hash map 
-            vector<int> hash1(26, 0);
-            for (char c : strs[i]) hash1[c - 'a']++;
-            for (int j = i + 1; j < n; j++) {
-                // put everything into the second map 
-                if (!used[j] && strs[i].size() == strs[j].size()) {
-                    vector<int> hash2(26, 0);
-                    for (int k = 0; k < (int)strs[i].size(); k++) {
-                        hash2[strs[j][k] - 'a']++;
-                    }
-                    if (hash1 == hash2) {
-                        temp.push_back(strs[j]);
-                        used[j] = 1;
-                    }
-                }
+        int n=strs.size();
+        map<vector<int>,vector<string>> mpp;
+        for(int i=0;i<n;i++){
+            //create a hash map 
+            vector<int> hash(26,0);
+            for(int j=0;j<strs[i].size();j++){
+                hash[strs[i][j]-'a']++;
             }
-            used[i] = 1;
-            ans.push_back(temp);
+                mpp[hash].push_back(strs[i]);
+        }
+        vector<vector<string>> ans;
+        for(auto it:mpp){
+            ans.push_back(it.second);
         }
         return ans;
     }
