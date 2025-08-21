5ad50e6287b8412b3e3040b8bdae80abe6d477af
SHRIYA DEVARAKONDA <142150657+shrrrriya@users.noreply.github.com>
2025-08-22 01:07:15 +0530

    Time: 3 ms (87.39%), Space: 128.5 MB (41.53%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n=nums.size();
-        queue<int> pos;
-        queue<int> neg;
+        vector<int> ans(n,0);
+        int pos=0; int neg=1;
         for(int i=0;i<n;i++){
-            if(nums[i]>0) pos.push(nums[i]);
-            if(nums[i]<0) neg.push(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            if(i%2==0){
-                nums[i]=pos.front();
-                pos.pop();
-            }
-            else{
-                nums[i]=neg.front();
-                neg.pop();
+            if(nums[i]>0){
+                ans[pos]=nums[i]; pos=pos+2;
+            }else{
+                ans[neg]=nums[i]; neg=neg+2;
             }
         }
-        return nums;
+        return ans;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]>0) pos.push(nums[i]);
+        //     if(nums[i]<0) neg.push(nums[i]);
+        // }
+        // for(int i=0;i<n;i++){
+        //     if(i%2==0){
+        //         nums[i]=pos.front();
+        //         pos.pop();
+        //     }
+        //     else{
+        //         nums[i]=neg.front();
+        //         neg.pop();
+        //     }
+        // }
+
     }
 };
